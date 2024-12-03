2e4e18df22bf95cacf86d4cfb224273de0ab99b6
Ivan Zivkovic <imzivko@gmail.com>
2024-12-03 14:12:53 +0100

    day-01

--- day-01/main.py
+++ day-01/main.py
@@ -1,31 +1,32 @@
 from pathlib import Path
 
-p = Path('./input.txt')
-f = p.read_text()
+f = Path('./input.txt')
+lines = f.read_text().strip().split()
 
-input_data = f.strip().split()
+def main(data):
+    left = []
+    right = []
 
-left = []
-right = []
+    for idx, num in enumerate(data):
+        if idx % 2 == 0:
+            left.append(num)
+        else:
+            right.append(num)
 
-for idx, num in enumerate(input_data):
-    if idx % 2 == 0:
-        left.append(num)
-    else:
-        right.append(num)
+    left.sort()
+    right.sort()
 
-left.sort()
-right.sort()
+    ans = 0
 
-out = 0
+    for first, second in zip(left, right):
+        # part one
+        # ans += abs(int(first) - int(second))
 
-for first, second in zip(left, right):
-    # part one
-    # out += abs(int(first) - int(second))
+        # part two
+        ans += abs(int(first) * right.count(first))
 
-    # part two
-    out += abs(int(first) * right.count(first))
+    return ans
 
+result = main(lines)
 
-print(out)
-
+print(result)
